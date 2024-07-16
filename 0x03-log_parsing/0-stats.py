9454fcee238944156965aff7cecdf5007067e391
yusrA312 <yusra2yahya@gmail.com>
2024-07-16 04:32:59 -0700

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,54 +5,54 @@ parsing function
 import sys
 
 
-counters = {
+count = {
     "size": 0,
     "lines": 1
 }
 
-cntCode = {
+sCode = {
     "200": 0, "301": 0, "400": 0, "401": 0,
     "403": 0, "404": 0, "405": 0, "500": 0
 }
 
 
-def printCodes():
+def Codes():
     """
     function to print the codes and the number of ocurrence
     """
-    # print file size
-    print("File size: {}".format(counters["size"]))
-    # print all codes
-    for key in sorted(cntCode.keys()):
-        # if a val is not 0
-        if cntCode[key] != 0:
-            print("{}: {}".format(key, cntCode[key]))
+    
+    print("File size: {}".format(count["size"]))
+    
+    for key in sorted(sCode.keys()):
+        
+        if sCode[key] != 0:
+            print("{}: {}".format(key, sCode[key]))
 
 
-def countCodeSize(listData):
+def CodeSize(listData):
     """
     count the codes and file size
     """
-    # count file size
-    counters["size"] += int(listData[-1])
-    # if exists the code
-    if listData[-2] in cntCode:
-        # count status code
-        cntCode[listData[-2]] += 1
-        # line 10 print
+    
+    count["size"] += int(listData[-1])
+   
+    if listData[-2] in sCode:
+        
+        sCode[listData[-2]] += 1
+        
 
 
 if __name__ == "__main__":
     try:
         for line in sys.stdin:
             try:
-                countCodeSize(line.split(" "))
+                CodeSize(line.split(" "))
             except:
                 pass
-            if counters["lines"] % 10 == 0:
-                printCodes()
-            counters["lines"] += 1
+            if count["lines"] % 10 == 0:
+                Codes()
+            count["lines"] += 1
     except KeyboardInterrupt:
-        printCodes()
+        Codes()
         raise
-    printCodes()
+    Codes()
