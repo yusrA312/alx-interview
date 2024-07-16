3abb43a8f5d5cb5a2529208c87bca1d164704970
yusrA312 <yusra2yahya@gmail.com>
2024-07-16 04:20:02 -0700

    done

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+A script for parsing HTTP request logs.
+"""
+import sys
+
+
+coun = {
+    "size": 0,
+    "lines": 1
+}
+
+cntCode = {
+    "200": 0, "301": 0, "400": 0, "401": 0,
+    "403": 0, "404": 0, "405": 0, "500": 0
+}
+
+
+def Codes():
+    """
+    function to print the codes and the number of ocurrence
+    """
+   
+    print("File size: {}".format(counters["size"]))
+   
+    for key in sorted(sCode.keys()):
+       
+        if sCode[key] != 0:
+            print("{}: {}".format(key, sCode[key]))
+
+
+def CodeSize(listData):
+    """
+    count the codes and file size
+    """
+    
+    coun["size"] += int(listData[-1])
+ 
+    if listData[-2] in sCode:
+ 
+        sCode[listData[-2]] += 1
+      
+
+
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            try:
+                CodeSize(line.split(" "))
+            except:
+                pass
+            if coun["lines"] % 10 == 0:
+              Codes()
+            coun["lines"] += 1
+    except KeyboardInterrupt:
+        Codes()
+        raise
+    Codes()
